8e9dacfd9aaa51598d26ef458c9db87bd504f057
ktoan <nktoanwork@gmail.com>
2022-07-02 12:07:34 +0700

    Server

--- server/index.js
+++ server/index.js
@@ -10,10 +10,13 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(
+      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ruedw.mongodb.net/?retryWrites=true&w=majority`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
 
     console.log("MongoDB Connected");
   } catch (error) {
